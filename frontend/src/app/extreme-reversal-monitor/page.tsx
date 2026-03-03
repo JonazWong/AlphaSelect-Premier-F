b1b55c0200fc87d6ea91cd591da41f8e1360f330
Jonazllion <143256272+JonazWong@users.noreply.github.com>
2026-03-04 02:40:49 +0800

    Add ExtremeReversalMonitor Page

---
+++ frontend/src/app/extreme-reversal-monitor/page.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+import { Card } from '../components/Card';
+import { Title } from '../components/Title';
+import { GlassCard } from '../components/GlassCard';
+
+const ExtremeReversalMonitor: FC = () => {
+  return (
+    <GlassCard>
+      <Title>Extreme Reversal Monitor</Title>
+      {/* Implement the UI elements according to design here */}
+      <p>This is where the Extreme Reversal Monitor UI will be implemented.</p>
+    </GlassCard>
+  );
+};
+
+export default ExtremeReversalMonitor;
